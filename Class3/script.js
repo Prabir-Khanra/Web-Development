83fa9037c6314da0db04d7b50cb16834225a81a4
Prabir <prabir123khanra@gmail.com>
2023-02-27 21:23:44 +0530

    Javascript Tutorial

--- Class3/script.js
+++ Class3/script.js
@@ -1,2 +1,27 @@
-console.log("Hello Programmer");
-console.log(35 + 49)
+// print
+console.log("Hello Champions")
+console.warn("Hey! programmer.. are you feeling ok?")
+console.error("Sorry! your action is not acceptable.")
+
+// Name = Hrittik Roshan
+// age = 46
+// proffession = Actor
+
+
+// string
+var name = "Soumitra Samanta"
+var name2 = 'Jidan Waresi'
+var x = "Ashish Sahu"
+
+console.log(name)
+console.log(name2)
+console.log(x)
+console.log("Prabir Khanra")
+
+
+// int (integer)
+var roll_no = 34
+var age = 47
+var m = roll_no + age
+console.log(roll_no)
+console.log(m)
