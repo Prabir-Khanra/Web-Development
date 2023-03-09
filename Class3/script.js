e879c72bfa99155088d3ef55688fa3e2a8218ebf
Prabir <prabir123khanra@gmail.com>
2023-03-09 21:34:07 +0530

    Javascript function

--- Class3/script.js
+++ Class3/script.js
@@ -1,5 +1,7 @@
 // print
-console.log("Hello Champions")
+console.log("Hello Champions") // javascript
+// print("Hello Champions")// python
+
 console.warn("Hey! programmer.. are you feeling ok?")
 console.error("Sorry! your action is not acceptable.")
 
@@ -25,3 +27,24 @@ var age = 47
 var m = roll_no + age
 console.log(roll_no)
 console.log(m)
+
+
+// float
+var price = 50.5
+var height = 5.7
+var x = 45.00
+
+var a = price + age
+// console.log(a) 
+// console.log(a) 
+// console.log(a) 
+
+
+function getName () {
+    console.log(a)
+    console.log("Awesome Coder")
+}
+
+getName()
+getName()
+getName()
