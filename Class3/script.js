be66a0bf2cf9f9401a2d607c62b7194105668156
Prabir <prabir123khanra@gmail.com>
2023-04-29 21:53:36 +0530

    javascript second class

--- Class3/script.js
+++ Class3/script.js
@@ -9,42 +9,100 @@ console.error("Sorry! your action is not acceptable.")
 // age = 46
 // proffession = Actor
 
+// "I am Prabir Khanra" ===> String 
+// 5.7 ===>  float  
+// 10   ====> integer (int)
+// 'soumitra 101' ===> String
+// 5.7   ===> float / double
+// '5.7' ===> string
+
+// 12   ===> integer 
+// 12.0  ===> float
+
 
 // string
 var name = "Soumitra Samanta"
 var name2 = 'Jidan Waresi'
 var x = "Ashish Sahu"
+var price = 98
 
-console.log(name)
-console.log(name2)
-console.log(x)
-console.log("Prabir Khanra")
-
-
-// int (integer)
-var roll_no = 34
-var age = 47
-var m = roll_no + age
-console.log(roll_no)
-console.log(m)
+// console.log(name)
+// console.log(name2)
+// console.log(x)
+// console.log("Prabir Khanra")
 
 
-// float
-var price = 50.5
-var height = 5.7
-var x = 45.00
-
-var a = price + age
-// console.log(a) 
-// console.log(a) 
-// console.log(a) 
+// // int (integer)
+// var roll_no = 34
+// var age = 47
+// var m = roll_no + age
+// console.log(roll_no)
+// console.log(m)
 
 
+// // float
+// var price = 50.5
+// var height = 5.7
+// var x = 45.00
+
+// var a = price + height
+// var b = price - height
+// console.log() 
+// console.log(b)
+// // console.log(a) 
+// // console.log(a) 
+
+
+// function 
 function getName () {
-    console.log(a)
     console.log("Awesome Coder")
+    var price1 = 12
+    var price2 = 35
+    var total_price = price1 + price2
+    console.log(total_price)
 }
 
-getName()
-getName()
-getName()
+function showPrice () {
+    console.log('show price')
+}
+
+function payMoney () {
+    console.log('pay money')
+}
+
+// getName()
+// showPrice()
+
+// getName()
+// getName()
+// getName()
+
+// ============================ add integer value
+// var j = 23
+// var k = 34
+// var m = j + k 
+// console.log(m)  // 57
+// console.log(j+k) // 57
+
+// ============================ add string value 
+// var n = "Hello"
+// var r = " "
+// var p = "Champion"
+// console.log(n+r+p)
+
+//  2 + 3 = 5                           2 + 3.5 = 5.5
+// var s = 2 
+// var t = 45.5
+// console.log(s+t)
+
+// alert  ====> show alert message
+// alert("Do you want to exit?")
+
+
+// confirm  ===> take confirmation
+// confirm("Do you really confirm this page ?")
+
+
+// prompt =====> take input from user with a dialog box
+prompt("What is your name?")
+
