7e8c7d47c8af3d85916ba40e56dce122f5cb47db
Prabir <prabir123khanra@gmail.com>
2023-06-02 21:51:02 +0530

    ES6 first class

---
+++ ES6/index.js
@@ -0,0 +1,39 @@
+const address = "Kolkata" // one time declare and one time initialize
+
+// ====================== re-declare ===============================
+// =================================================================
+// possible for var ====================
+var name = "Arijit Sing"
+var name = "Shreya Ghoshal"
+console.log(name)
+
+// impossible for let ==================
+// let name = "Arijit Sing"
+// let name = "Shreya Ghoshal"
+// console.log(name)
+
+// impossible for const ==================
+// const name = "Arijit Sing"
+// const name = "Shreya Ghoshal"
+// console.log(name)
+
+
+
+
+// ===================== re-initialize ==============================
+// ==================================================================
+
+// possible for var ===================
+var about = "Singer"
+about = "Dancer"
+console.log(about)
+
+// possible for let ===================
+// let about = "Singer"
+// about = "Dancer"
+// console.log(about)
+
+// impossible for const ===============
+// const about = "Singer"
+// about = "Dancer"
+// console.log(about)
