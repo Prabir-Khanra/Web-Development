02691258a8428a3b0be7abecf8ced0ccf37558cc
Prabir <prabir123khanra@gmail.com>
2023-06-04 21:41:31 +0530

    array destructuring

---
+++ ES6/index2.js
@@ -0,0 +1,14 @@
+function showName() {
+    console.log("I am a Ethical Hacker")
+} 
+showName() // call
+
+var name = "Shahrukh Khan"
+let abot = "Shahrukh Khan"
+const address = "Shahrukh Khan"
+
+// arrow function
+var showName = () => {
+    console.log("I am a Ethical Hacker")
+}
+showName()
---
+++ ES6/index3.js
@@ -0,0 +1,12 @@
+var friends = ["Hrittik","Shahrukh","Salman","Akshay Kumar","Sunny Deol"] // array
+var age = [34,57,89,23,12,51]
+
+// var newArray = friends.concat(age)
+// console.log(newArray)
+
+// var newArray = [...friends,...age]
+// console.log(newArray)
+
+for(var item of friends) { // shahrukh
+    console.log(item)
+}
