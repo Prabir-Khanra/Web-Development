bde1cdce9b1bfda6c1188f22ac8d28df5c624ca9
Prabir <prabir123khanra@gmail.com>
2023-06-13 21:59:01 +0530

    De-structure

---
+++ ES6/index4.js
@@ -0,0 +1,13 @@
+// de-structure
+var super_human = {
+    "height":5.6,
+    "skin_color": 'fair',
+    "name": 'Johny Sins',
+    "proffession": 'Doctor',
+    "follower" : '750cr'
+}
+// console.log(super_human)
+// console.log(super_human.name)
+var {follower,name,proffession} = super_human
+console.log(name)
+console.log(follower)
